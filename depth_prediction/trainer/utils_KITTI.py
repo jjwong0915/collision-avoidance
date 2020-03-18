7d8d851caca59193d43aa225e0ae201478e7207c
YJ-Pan <55353607+YJ-Pan@users.noreply.github.com>
2020-03-18 20:54:13 +0800

    Add save_pfm

--- depth_prediction/trainer/utils_KITTI.py
+++ depth_prediction/trainer/utils_KITTI.py
@@ -488,6 +488,32 @@ def read_pfm(file):
 
     return data, scale
 
+# 儲存.pfm檔案
+def save_pfm(file, image, scale = 1):
+  color = None
+
+  if image.dtype.name != 'float32':
+    raise Exception('Image dtype must be float32.')
+
+  if len(image.shape) == 3 and image.shape[2] == 3: # color image
+    color = True
+  elif len(image.shape) == 2 or len(image.shape) == 3 and image.shape[2] == 1: # greyscale
+    color = False
+  else:
+    raise Exception('Image must have H x W x 3, H x W x 1 or H x W dimensions.')
+
+  file.write('PF\n' if color else 'Pf\n')
+  file.write('%d %d\n' % (image.shape[1], image.shape[0]))
+
+  endian = image.dtype.byteorder
+
+  if endian == '<' or endian == '=' and sys.byteorder == 'little':
+    scale = -scale
+
+  file.write('%f\n' % scale)
+
+  image.tofile(file) 
+
 # 讀取 AirSim Dataset 中的深度圖 (.pfm)
 def read_AirSim_depth(image_path, shape=(DEPTH_H, DEPTH_W)):
     DEPTH_H, DEPTH_W = shape
